fd78865ed14c7b723720483123ae82cb06d56b15
Matteo Cipolla <mati_teo@live.it>
2025-02-10 17:29:47 +0100

    feat:add header and homepage

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,17 +3,20 @@ import { NavLink } from "react-router-dom";
 export default function Header() {
   return (
     <header>
-      <nav>
+      <nav className="container">
         <ul>
-          <li>
-            <NavLink to="/">Homepage</NavLink>
-          </li>
-          <li>
-            <NavLink to="/about-us">Chi siamo</NavLink>
-          </li>
-          <li>
-            <NavLink to="/contacts">Contatti</NavLink>
-          </li>
+          <div className="logo">Blog 138</div>
+          <div className="nav-links">
+            <li>
+              <NavLink to="/">Homepage</NavLink>
+            </li>
+            <li>
+              <NavLink to="/about-us">Chi siamo</NavLink>
+            </li>
+            <li>
+              <NavLink to="/contacts">Contatti</NavLink>
+            </li>
+          </div>
         </ul>
       </nav>
     </header>
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,41 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
 export default function Homepage() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:3000/posts").then((response) => {
+      setPosts(response.data);
+    });
+  }, []);
+
   return (
-    <div>
-      <h2>Benvenuto nel nostro Blog!</h2>
+    <div className="container page-container">
+      <h2 className="page-title">Benvenuto nel nostro Blog!</h2>
       <p>Scopri gli ultimi articoli e curiosità.</p>
+      <div className="posts-wrap">
+        {posts.map((post) => (
+          <div key={post.id} className="post-card">
+            <img src={post.image} alt={post.title} />
+            <div className="post-content">
+              <h3 className="post-title">{post.title}</h3>
+              <p className="post-description">{post.content}</p>
+              <div className="tags">
+                {Array.isArray(post.tags) ? (
+                  post.tags.map((tag, index) => (
+                    <div key={index} className="tag">
+                      {tag}
+                    </div>
+                  ))
+                ) : (
+                  <div className="tag">{post.tags}</div>
+                )}
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
